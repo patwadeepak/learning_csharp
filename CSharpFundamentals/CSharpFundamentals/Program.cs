d462bacc9b2f9ad5c6864c1c923653fd95fe3821
Deepak Kumar <patwa.deepak@gmail.com>
2020-11-15 15:47:32 +0530

    Random Number Generator coded done.

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment to run any program.
+            RandomNumberGenerator.Run();
+        }
+    }
+}
---
+++ CSharpFundamentals/CSharpFundamentals/RandomNumberGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class RandomNumberGenerator
+    {
+        public static void Run()
+        {
+            Console.Write("Enter the length of password needed: ");
+            var length = int.Parse(Console.ReadLine());
+
+            var random = new Random();
+            int charNum;
+            char character;
+            for(var i=0; i<length; i++)
+            {
+                charNum = random.Next(0, 63);
+                
+                if (charNum < 26)
+                {
+                    character = (char)(charNum + (int)'a');
+                    Console.Write(character);
+                }
+                else if (charNum < 52)
+                {
+                    character = (char)(charNum - 26 + (int)'A');
+                    Console.Write(character);
+                }
+                else
+                {
+                    charNum = charNum - 52;
+                    Console.Write(charNum);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
